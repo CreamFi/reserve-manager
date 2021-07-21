46f895d781109fd8e7690aa883ce91cc6b3af6cc
bun919tw <bun919tw@gmail.com>
2021-07-21 12:22:31 +0800

    *: init project

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: {
+    version: "0.8.2" ,
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+};
